22d4ecabc0b87bf196dce1bbeba5459df8ea9639
joshcyu <joshuafermin003@gmail.com>
2024-10-06 12:54:20 +0800

    added admin cancel button, modified z of admin container

--- src/CMAccess.jsx
+++ src/CMAccess.jsx
@@ -6,18 +6,22 @@ export default function CMAccess(props) {
 
 
   return (
-    <div className='fixed flex justify-center items-center w-[100%] h-[100%] top-0 left-0 right-0 bottom-0 bg-gray-500/25 z-[2] text-copythirtiary'>
+    <div className='fixed flex justify-center items-center w-[100%] h-[100%] top-0 left-0 right-0 bottom-0 bg-gray-500/25 z-40 text-copythirtiary'>
       <div className='w-[90%] sm:w-[30%] h-[300px] sm:h-[200px] bg-grape flex flex-col gap-4 justify-center items-center rounded-sm'>
         <p className='text-2xl text-center'>Content Management Access</p>
         <input type='text' placeholder='Enter Admin Code' className='bg-transparent focus:outline-none border-b-2' onChange={(e) => {
           const newCode = e.target.value
           setCode(newCode)
         }}/>
-        <button className='px-4 py-2 bg-grape2 rounded-lg duration-200 hover:opacity-80' onClick={() => {
-          handleAdminAccess(code)
-          handleContent()
-        }}>Submit</button>
-
+        <div className='flex gap-5'>
+          <button className='px-4 py-2 bg-grape2 rounded-lg duration-200 hover:opacity-80' onClick={() => {
+            handleAdminAccess(code)
+            handleContent()
+          }}>Submit</button>
+          <button className='hover:opacity-80 bg-copythirtiary rounded-lg text-grape px-4 py-2' onClick={() => {
+            handleContent()
+          }}>Cancel</button>
+        </div>
         <p className=''>{children}</p>
       </div>
     </div>
